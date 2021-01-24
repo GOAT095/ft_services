e9d1dc7cf8e788d667471e88981cbae13c6b01bf
Abdelhakim Nassif <anassif@e2r1p8.1337.ma>
2021-01-24 16:37:50 +0100

    reverse proxy is in

--- srcs/phpmyadmin/phpmyadmin-script.sh
+++ srcs/phpmyadmin/phpmyadmin-script.sh
@@ -2,4 +2,5 @@ sudo openrc reboot
 rc-service nginx start
 rc-service php-fpm7 start
 /telegraf-1.17.0/usr/bin/telegraf --config /etc/telegraf/telegraf.conf &
+#to keep process running
 tail -F /dev/null
--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@ minikube start
 eval $(minikube -p minikube docker-env)
 
 #docker build -t local-nginx srcs/Nginx/
-#docker build -t local-mysql srcs/MySQL/
+docker build -t local-mysql srcs/MySQL/
 #docker build -t local-wordpress srcs/WordPress/
-#docker build -t local-phpmyadmin srcs/phpMyAdmin/
+docker build -t local-phpmyadmin srcs/phpMyAdmin/
 #docker build -t local-influxdb srcs/InfluxDB
 #docker build -t local-grafana srcs/Grafana
-docker build -t local-ftps srcs/ftps
+#docker build -t local-ftps srcs/ftps
 
 # 1 - MetalLB Installation
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
@@ -24,11 +24,11 @@ kubectl apply -f srcs/metallb-configmap.yaml
 # 2 - Nginx / MySQL / Wordpress
 
 #kubectl apply -f srcs/nginx-deplsvc.yaml
-#kubectl apply -f srcs/mysql-deplsvc.yaml
+kubectl apply -f srcs/mysql-deplsvc.yaml
 #kubectl apply -f srcs/wordpress-deplsvc.yaml
-#kubectl apply -f srcs/phpmyadmin-deplsvc.yaml
+kubectl apply -f srcs/phpmyadmin-deplsvc.yaml
 #kubectl apply -f srcs/influxdb-deplsvc.yaml
 #kubectl apply -f srcs/grafana-deplsvc.yaml
-kubectl apply -f srcs/ftps-deplsvc.yaml
+#kubectl apply -f srcs/ftps-deplsvc.yaml
 kubectl get pods
 minikube dashboard
