e310c502fb432f1fcdf9d1b668ee7ed5ae70ec65
Abdelhakim Nassif <anassif@e2r1p8.1337.ma>
2021-01-27 16:32:51 +0100

    grafana

--- srcs/phpmyadmin/phpmyadmin-script.sh
+++ srcs/phpmyadmin/phpmyadmin-script.sh
@@ -1,8 +1,4 @@
 sudo openrc reboot
 rc-service nginx start
 rc-service php-fpm7 start
-#/telegraf-1.17.0/usr/bin/telegraf --config /etc/telegraf/telegraf.conf &
-
 #to keep process running
-#tail -F /dev/null
-exec top
