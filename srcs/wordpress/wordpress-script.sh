6e1b55ee09aa0c3ac1c49b9a115fee6704896ee4
Abdelhakim Nassif <anassif@e2r1p8.1337.ma>
2021-01-27 17:47:22 +0100

    graphs on

--- srcs/wordpress/wordpress-script.sh
+++ srcs/wordpress/wordpress-script.sh
@@ -1,5 +1,5 @@
 sudo openrc reboot
 rc-service nginx start
 rc-service php-fpm7 start
-/telegraf-1.17.0/usr/bin/telegraf --config /etc/telegraf/telegraf.conf &
-tail -F /dev/null
+/telegraf/telegraf --config /etc/telegraf/telegraf.conf &
+exec top
