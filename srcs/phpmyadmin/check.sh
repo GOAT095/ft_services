fdcf0201fdbee92d3b304147bb76bc2054604c59
Abdelhakim Nassif <anassif@e2r1p8.1337.ma>
2021-01-31 15:24:14 +0100

    liveness on phpmyadmin

--- srcs/phpmyadmin/check.sh
+++ srcs/phpmyadmin/check.sh
@@ -7,7 +7,7 @@ ps_3=$?
 ps > ps_log
 grep '\[*\]' ps_log
 ps_log=$?
-if [ $ps_1 -eq 0 ] && $ps_1 -eq 0 ] && [ $ps_3 -eq 0 ] && [ $ps_log -eq 1 ]; then
+if [ $ps_1 -eq 0 ] && [ $ps_1 -eq 0 ] && [ $ps_3 -eq 0 ] && [ $ps_log -eq 1 ]; then
         exit 0;
 fi
 exit 1;
